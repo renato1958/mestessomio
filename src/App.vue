bf5a575c6ffb2ef100b5bed8cca89b69854f608c
renato1958 <felisi.renato@gmail.com>
2021-10-12 14:49:24 +0200

    Modificato un errore di layout e aggiunti stili

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,19 @@ export default {
 <style>
 #navigation {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+}
+
+#navigation a:link,
+a:visited {
+  color: #223344;
+  text-decoration: none;
+}
+
+@media only screen and (min-width: 700px) {
+  #navigation a:hover {
+    color: rebeccapurple;
+    text-decoration: underline;
+  }
 }
 </style>
--- src/views/TheNavigation.vue
+++ src/views/TheNavigation.vue
@@ -1,7 +1,11 @@
 <template>
   <h1>Rassegna di libri</h1>
-  <div id="navigation" v-for="book in books" :key="book.id">
-    <router-link :to="`/libri/${book.url}`">
+  <div id="navigation">
+    <router-link
+      v-for="book in books"
+      :key="book.id"
+      :to="`/libri/${book.url}`"
+    >
       <h3>{{ book.title }}</h3>
     </router-link>
   </div>
